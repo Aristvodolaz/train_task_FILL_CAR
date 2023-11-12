8c30e70436b5b67b1b01354d28f943a108689f4e
Aristvodolaz <kap.moral22@gmail.com>
2023-11-12 14:13:19 +0300

    {2} - create nav menu and add click image / info fragment

--- app/src/main/java/com/example/train_task_fill_car/activity/MainActivity.java
+++ app/src/main/java/com/example/train_task_fill_car/activity/MainActivity.java
@@ -1,16 +1,35 @@
 package com.example.train_task_fill_car.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.train_task_fill_car.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.code) ImageView code;
+    @BindView(R.id.info) ImageView info;
+    @BindView(R.id.person) ImageView person;
+    @Override
+    protected void initViews(@Nullable Bundle saveInstanceState) {
+        code.setOnClickListener(l->{startActivity(new Intent(this, CameraActivity.class));});
+        info.setOnClickListener(l->{});
+        person.setOnClickListener(l->{});
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected int layoutResId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected int titleResId() {
+        return 0;
     }
 }
---
+++ app/src/main/java/com/example/train_task_fill_car/fragment/InfoFragment.java
@@ -0,0 +1,13 @@
+package com.example.train_task_fill_car.fragment;
+
+import android.os.Bundle;
+
+public class InfoFragment extends BaseFragment{
+    public static InfoFragment newInstance() {
+        return new InfoFragment();
+    }
+    @Override
+    protected int layoutId() {
+        return 0;
+    }
+}
