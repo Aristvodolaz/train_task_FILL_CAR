148d3e249eafd7a8e0d1406f46de38985bfbc35b
Aristvodolaz <kap.moral22@gmail.com>
2023-11-12 13:50:18 +0300

    {1} - add activity with camera

--- app/src/main/java/com/example/train_task_fill_car/fragment/PayFragment.java
+++ app/src/main/java/com/example/train_task_fill_car/fragment/PayFragment.java
@@ -1,4 +1,21 @@
 package com.example.train_task_fill_car.fragment;
 
-public class PayFragment {
+import android.os.Bundle;
+
+public class PayFragment extends BaseFragment{
+
+    private String colonka;
+
+    public PayFragment(String colonka) {
+        this.colonka = colonka;
+    }
+
+    public static PayFragment newInstance(String colonka) {
+        PayFragment fragment = new PayFragment(colonka);
+        return fragment;
+    }
+    @Override
+    protected int layoutId() {
+        return 0;
+    }
 }
